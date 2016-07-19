ec561186d24774117a44de0f861c54710fc5da35
Alyson Takai <shtakai@gmail.com>
2016-07-19 16:47:00 -0700

    login function

--- client/assets/controllers/login.controller.js
+++ client/assets/controllers/login.controller.js
@@ -3,5 +3,16 @@ console.log('LoginController')
 angular.module('app').controller(
   'LoginController', function(LoginFactory, $location, $routeParams) {
     console.log('C start')
+
+    this.login = () => {
+      this.email = this.email || ''
+      this.password = this.password || ''
+      console.log('C login', 'email', this.email,'password', this.password)
+
+      LoginFactory.login(this.email, this.password, (data) => {
+         console.log('returned data', data)
+      })
+      consnole.log('C login end')
+    }
   }
 )
