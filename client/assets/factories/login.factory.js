5c0680d2fef309a19c50052979a03e05d38ea106
Alyson Takai <shtakai@gmail.com>
2016-07-19 16:33:02 -0700

    create base

--- client/assets/factories/login.factory.js
+++ client/assets/factories/login.factory.js
@@ -1,1 +1,16 @@
 console.log('LoginFactory')
+
+angular.module('app').factory(
+  'LoginFactory',
+  function($http) {
+    console.log('F start')
+
+    let factory = {}
+
+
+
+
+    return factory
+
+  }
+)
