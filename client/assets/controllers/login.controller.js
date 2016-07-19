dfade2273ad176179237356da536780777e8aa84
Alyson Takai <shtakai@gmail.com>
2016-07-19 16:32:43 -0700

    inject factory

--- client/assets/controllers/login.controller.js
+++ client/assets/controllers/login.controller.js
@@ -1,7 +1,7 @@
 console.log('LoginController')
 
 angular.module('app').controller(
-  'LoginController', function($location, $routeParams) {
+  'LoginController', function(LoginFactory, $location, $routeParams) {
     console.log('C start')
   }
 )
