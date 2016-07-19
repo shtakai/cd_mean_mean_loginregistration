fb858e1f29b9e3055d548445857ed71f6a99ed2d
Alyson Takai <shtakai@gmail.com>
2016-07-19 14:15:49 -0700

    just create fake user adding

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const faker = require('faker')
 
 const app = express()
 
@@ -20,9 +21,27 @@ app.use(express.static(path.join(__dirname, './bower_components')))
 require('./server/config/connection.js')
 
 // load router
+const mongoose = require('mongoose')
+app.get('/', (req, res) => {
+  const User = mongoose.model('User')
+  let user = new User()
+  user.first_name = faker.commerce.productName()
+  user.last_name = faker.commerce.productName()
+  user.email = faker.internet.email()
+  user.password = faker.internet.password()
+
+  console.log('user', user)
+  user.save((err, _user) => {
+    if(err){ res.json(err) }
+    else{
+     console.log('saved', _user)
+      res.json({test:'test'})
+
+    }
+  })
 
 
-//app.get('/', (req, res) => {res.json({test:'test'})})
+  })
 
 
 // start server
