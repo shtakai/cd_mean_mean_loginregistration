3adfff1a4627047f18c448c7dcd35f015ba98970
Alyson Takai <shtakai@gmail.com>
2016-07-20 10:43:38 -0700

    register

--- client/assets/factories/login.factory.js
+++ client/assets/factories/login.factory.js
@@ -26,6 +26,39 @@ angular.module('app').factory(
       })
     }
 
+    factory.dashboard = (callback) =>{
+       console.log('F dashboard')
+       $http.get('/dashboard')
+         .then(data => {
+            console.log('data', data)
+         })
+       callback({success: true})
+    }
+
+    factory.register = (
+      email,
+      first_name,
+      last_name,
+      password,
+      password_confirm,
+      callback) => {
+        console.log('F register')
+        $http.post('/users',{
+          email: email,
+          first_name: first_name,
+          last_name: last_name,
+          password: password,
+          password_confirm:password_confirm
+        }).then( data => {
+           console.log(data)
+           if(data.data.success){
+            console.log('success')
+           }else {
+            console.log('failed')
+           }
+           callback(data.data)
+        } )
+      }
 
 
     return factory
